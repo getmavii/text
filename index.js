e00f8569ff25d907674f41f6988eb276196ff65b
Nick Chapman <nchapman@fastmail.com>
2023-02-08 11:47:14 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,46 @@
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { Readability } from "npm:@mozilla/readability";
+
+const USER_AGENT =
+  "Mozilla/5.0 (compatible; MaviiBot/1.0; +https://mavii.com/bots)";
+
+serve(async (req) => {
+  const url = new URL(req.url);
+  const pageUrl = url.searchParams.get("url");
+
+  let article = null;
+
+  try {
+    const res = await fetch(pageUrl, {
+      headers: {
+        "User-Agent": USER_AGENT,
+      },
+    });
+
+    if (res.ok) {
+      const body = await res.text();
+      const doc = new DOMParser().parseFromString(body, "text/html");
+
+      // Parse article
+      article = new Readability(doc).parse();
+
+      // Delete html content
+      delete article.content;
+    } else {
+      console.error(res.statusText);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  // Pretty print JSON
+  const json = JSON.stringify(article, null, 2);
+
+  return new Response(json, {
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "public, max-age=86400",
+    },
+  });
+});
