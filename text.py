c7dedd905cef445fd46af837ae58f6c6b1e0bf34
Nick Chapman <nchapman@fastmail.com>
2023-03-16 12:20:39 -0700

    Parse metadata.

--- text.py
+++ text.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 import requests
 import justext
+import metadata_parser
 
 app = Flask(__name__)
 
@@ -12,12 +13,32 @@ def index():
   if url is None:
     return { "error": "No URL provided" }
   else:
-    return { "text": get_text(url) }
+    return parse(url)
 
-def get_text(url):
+def parse(url):
   response = requests.get(url)
-  paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
-  text_paragraphs = [paragraph.text for paragraph in paragraphs if not paragraph.is_boilerplate]
+  content = response.content
 
-  # Join paragraphs together
+  text = parse_text(content)
+  metadata = parse_metadata(url, content)
+
+  return { "text": text, **metadata }
+
+def parse_text(content):
+  paragraphs = justext.justext(content, justext.get_stoplist("English"))
+  text_paragraphs = [paragraph.text for paragraph in paragraphs if not paragraph.is_boilerplate]
+  
   return "\n".join(text_paragraphs)
+
+def parse_metadata(url, content):
+  page = metadata_parser.MetadataParser(url=url, html=content)
+
+  return {
+    "url": page.get_discrete_url(),
+    "title": page.get_metadata("title"),
+    "site_name": page.get_metadata("site_name"),
+    "description": page.get_metadata("description"),
+    "image": page.get_metadata_link("image"),
+    "keywords": page.get_metadata("keywords"),
+    "author": page.get_metadata("author"),
+  }
